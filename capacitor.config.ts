3d8ec5b4a3ca11d18bf35896ea2edad99bb5876c
slayerkodi <slayerkodi@gmail.com>
2026-05-30 20:51:36 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.liveradio.app',
+  appName: 'Live Radio Station',
+  webDir: 'dist'
+};
+
+export default config;
